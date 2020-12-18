b6db5ca6fb9797855e94ba4b9d53454ce5359cb4
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2020-12-18 08:51:43 +0100

    Frm principal creado y diseño terminado

---
+++ SgRaquetaAlvaroMerino/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace SgRaquetaAlvaroMerino
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aDMINISTRARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sOCIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.pISTASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rESERVASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNFORMESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aDMINISTRARToolStripMenuItem,
+            this.rESERVASToolStripMenuItem,
+            this.iNFORMESToolStripMenuItem,
+            this.sALIRToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aDMINISTRARToolStripMenuItem
+            // 
+            this.aDMINISTRARToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sOCIOSToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.pISTASToolStripMenuItem});
+            this.aDMINISTRARToolStripMenuItem.Name = "aDMINISTRARToolStripMenuItem";
+            this.aDMINISTRARToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.aDMINISTRARToolStripMenuItem.Text = "ADMINISTRAR";
+            // 
+            // sOCIOSToolStripMenuItem
+            // 
+            this.sOCIOSToolStripMenuItem.Name = "sOCIOSToolStripMenuItem";
+            this.sOCIOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sOCIOSToolStripMenuItem.Text = "SOCIOS";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // pISTASToolStripMenuItem
+            // 
+            this.pISTASToolStripMenuItem.Name = "pISTASToolStripMenuItem";
+            this.pISTASToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pISTASToolStripMenuItem.Text = "PISTAS";
+            // 
+            // rESERVASToolStripMenuItem
+            // 
+            this.rESERVASToolStripMenuItem.Name = "rESERVASToolStripMenuItem";
+            this.rESERVASToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.rESERVASToolStripMenuItem.Text = "RESERVAS";
+            // 
+            // iNFORMESToolStripMenuItem
+            // 
+            this.iNFORMESToolStripMenuItem.Name = "iNFORMESToolStripMenuItem";
+            this.iNFORMESToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.iNFORMESToolStripMenuItem.Text = "INFORMES";
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sALIRToolStripMenuItem.Text = "SALIR";
+            this.sALIRToolStripMenuItem.Click += new System.EventHandler(this.sALIRToolStripMenuItem_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aDMINISTRARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sOCIOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem pISTASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rESERVASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNFORMESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+    }
+}
+
